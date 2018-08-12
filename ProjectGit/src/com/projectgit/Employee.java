a4cd671fc1273601463057eb9c62feb2880afb51
anurag0708 <anuragpandey.hjp@gmail.com>
2018-08-12 18:11:33 +0530

    Resolved Conflict

--- ProjectGit/src/com/projectgit/Employee.java
+++ ProjectGit/src/com/projectgit/Employee.java
@@ -17,7 +17,6 @@ public class Employee {
 		this.employeeName = employeeName;
 	}
 	@Override
-<<<<<<< HEAD
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
@@ -42,10 +41,9 @@ public class Employee {
 		} else if (!employeeName.equals(other.employeeName))
 			return false;
 		return true;
-=======
+	}
 	public String toString() {
 		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + "]";
->>>>>>> 11b0e600e78f878a8125cd57f266bdf2870af98f
 	}
 
 
