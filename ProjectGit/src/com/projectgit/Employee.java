c3ed72be3c6539a1779c0c122e39c27a6f9b1b5f
anurag0708 <anuragpandey.hjp@gmail.com>
2018-08-12 17:39:58 +0530

    Update the Emloyee class - Override hashcode and equal method

--- ProjectGit/src/com/projectgit/Employee.java
+++ ProjectGit/src/com/projectgit/Employee.java
@@ -16,6 +16,32 @@ public class Employee {
 	public void setEmployeeName(String employeeName) {
 		this.employeeName = employeeName;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + employeeId;
+		result = prime * result + ((employeeName == null) ? 0 : employeeName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (employeeId != other.employeeId)
+			return false;
+		if (employeeName == null) {
+			if (other.employeeName != null)
+				return false;
+		} else if (!employeeName.equals(other.employeeName))
+			return false;
+		return true;
+	}
 
 
 }
